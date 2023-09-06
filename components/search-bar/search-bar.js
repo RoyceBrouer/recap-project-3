bdcb0a57dd27b9d35d5d679e2f07c9e3ce81bd30
Royce Brouër <royce@systemli.org>
2023-09-06 14:54:44 +0200

    add eventlistener submit button

---
+++ components/search-bar/search-bar.js
@@ -0,0 +1,6 @@
+// import { searchQuery } from "../index.js";
+// const searchBar = document.querySelector('[data-js = "search-bar"]');
+
+// export function handleSearch() {}
+
+// searchBar.onSubmit = (event) => handleSearch();
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {createCharacterCard} from "./components/card/card.js"
+import { createCharacterCard } from "./components/card/card.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
@@ -13,43 +13,49 @@ const pagination = document.querySelector('[data-js="pagination"]');
 // States
 const maxPage = 1;
 const page = 1;
-const searchQuery = "";
+export const searchQuery = "";
 
-nextButton.addEventListener('click', () => {
+nextButton.addEventListener("click", () => {
   if (page < maxPage) {
     page++;
     fetchCharacters(page);
-    console.log("Wow Rick! i can see all the Characters.")
+    console.log("Wow Rick! i can see all the Characters.");
   } else {
-    alert("Stop Morty! 'burb' You can't go there, it's the end of the Page")
+    alert("Stop Morty! 'burb' You can't go there, it's the end of the Page");
   }
 });
 
-prevButton.addEventListener('click', () => {
+prevButton.addEventListener("click", () => {
   if (page > 1) {
     page--;
     fetchCharacters(page);
   } else {
-    alert("Aw Jeez Rick. You you you cant't go back any further. Let's go home Rick. I miss Jessica");
+    alert(
+      "Aw Jeez Rick. You you you cant't go back any further. Let's go home Rick. I miss Jessica"
+    );
   }
 });
 
 export async function fetchCharacters() {
   try {
-    const response = await fetch("https://rickandmortyapi.com/")
-    if (response.ok){
+    const response = await fetch("https://rickandmortyapi.com/");
+    if (response.ok) {
       const data = await response.json();
-      cardContainer.innerHTML = '';
+      cardContainer.innerHTML = "";
       data.slice(0, 20).forEach((character) => {
         const card = createCharacterCard(character);
         cardContainer.append(card);
       });
     } else {
       console.log("Oh jeez...");
-    } 
-     }catch(error) {
-     console.error("oh jeez Rick. I don't know.");
     }
+  } catch (error) {
+    console.error("oh jeez Rick. I don't know.");
+  }
 }
 fetchCharacters();
 
+searchBar.addEventListener("submit", () => {
+  searchQuery = event.target;
+  console.log(searchQuery);
+});

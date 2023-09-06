4ab2da4dc8e9af943e8574b87ae84a3c60bb7b83
Royce Brouër <royce@systemli.org>
2023-09-06 11:17:17 +0200

    innerHTML createCard function

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,28 @@
-export function createCharacterCard() {}
+import 
+const cardcontainer = document.querySelector('[data-js="card-container"]');
+
+export async function createCharacterCard() {
+    const image = await 
+  const characterCard = document.createElement(li); //document?
+  characterCard.classList.add("card");
+  characterCard.innerHTML = `div class="card__image-container">
+  <img
+    class="card__image"
+    src="${}"
+    alt="${}"
+  />
+  <div class="card__image-gradient"></div>
+</div>
+<div class="card__content">
+  <h2 class="card__title">${}</h2>
+  <dl class="card__info">
+    <dt class="card__info-title">Status</dt>
+    <dd class="card__info-description">${}</dd>
+    <dt class="card__info-title">Type</dt>
+    <dd class="card__info-description">${}</dd>
+    <dt class="card__info-title">Occurrences</dt>
+    <dd class="card__info-description">${}</dd>
+  </dl>
+</div>`;
+cardcontainer.append(characterCard);
+}

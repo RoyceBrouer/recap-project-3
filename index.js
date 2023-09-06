d674f54dcfeae7ece83085246cb3e0048f66b7d8
Royce Brouër <royce@systemli.org>
2023-09-06 15:43:20 +0200

    updated searchQuery

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import {createCharacterCard} from "./components/card/card.js"
+import { createCharacterCard } from "./components/card/card.js";
 // States
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
-  );
+);
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
@@ -13,46 +13,53 @@ const pagination = document.querySelector('[data-js="pagination"]');
 
 let maxPage = 1;
 let page = 1;
-const searchQuery = "";
+let searchQuery = "";
 
-nextButton.addEventListener('click', () => {
+nextButton.addEventListener("click", () => {
   if (page < maxPage) {
     page++;
     fetchCharacters(page);
-    console.log("Wow Rick! i can see all the Characters.")
+    console.log("Wow Rick! i can see all the Characters.");
   } else {
-    alert("Stop Morty! 'burb' You can't go there, it's the end of the Page")
+    alert("Stop Morty! 'burb' You can't go there, it's the end of the Page");
   }
 });
 
-prevButton.addEventListener('click', () => {
+prevButton.addEventListener("click", () => {
   if (page > 1) {
     page--;
     fetchCharacters(page);
   } else {
-    alert("Aw Jeez Rick. You you you cant't go back any further. Let's go home Rick. I miss Jessica");
+    alert(
+      "Aw Jeez Rick. You you you cant't go back any further. Let's go home Rick. I miss Jessica"
+    );
   }
 });
 
 export async function fetchCharacters(page) {
   try {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}&name=${searchQuery}`);
-      const data = await response.json();
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/?page=${page}&name=${searchQuery}`
+    );
+    const data = await response.json();
 
     maxPage = data.info.pages;
     pagination.textContent = `${page} / ${maxPage}`;
-if (searchQuery){
-  page=1;
-}
-      cardContainer.innerHTML = '';
-      data.results.forEach((character) => {
-        const card = createCharacterCard(character);
-        cardContainer.append(card);
-        
-      });
-    
-     }catch(error) {
-     console.error("aw jeez Rick. I don't know.", error);
+    if (searchQuery) {
+      page = 1;
     }
+    cardContainer.innerHTML = "";
+    data.results.forEach((character) => {
+      const card = createCharacterCard(character);
+      cardContainer.append(card);
+    });
+  } catch (error) {
+    console.error("aw jeez Rick. I don't know.", error);
+  }
 }
 fetchCharacters(page);
+
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchQuery = event.target.elements.query.value;
+});
